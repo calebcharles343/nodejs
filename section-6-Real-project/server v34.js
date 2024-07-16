2026eba39dfec585d304b9822a6008c090fd3120
Charles Caleb <charlesofficial9@gmail.com>
2024-07-16 08:19:02 +0100

    login & logout with express app

--- section-6-Real-project/server v34.js
+++ section-6-Real-project/server v34.js
@@ -1,32 +1,14 @@
 /* Understanding Cookies */
 //Note: npm i cookie-parser
 
-////////////////////////
-//Require module
-////////////////////////
 const express = require("express");
 const path = require("path");
-const bodyparser = require("body-parser");
-const session = require("express-session");
-const { v4: uuidv4 } = require("uuid");
-
-const router = require("./router");
-
-////////////////////////////////
-//Create instance of the modules
-////////////////////////////////
-
 const app = express();
+
 ////////////////////////////
 //SPECIFY PORT
 ///////////////////////////
 const port = process.env.PORT || 3000;
-
-/////////////////////////////////////////
-//To recieve data in formatted form(json)
-/////////////////////////////////////////
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
 
 ////////////////////////////////////
 //Initialise view engine: ejs template
@@ -38,21 +20,6 @@ app.set("view engine", "ejs");
 ///////////////////////////
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
-/////////////////////////////////////
-//create session with uuid unique key
-/////////////////////////////////////
-
-app.use(
-  session({
-    // secret: "secret",
-    secret: uuidv4(),
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.use("/route", router); //to add all routes in the router.js file
 ///////////////
 //DATA
 ///////////////
